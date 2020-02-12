094c243f91fa3ccdcf4b4fdaf7a6edba41bff1d0
henriqueserra <52676788+henriqueserra@users.noreply.github.com>
2020-02-12 18:39:04 -0300

    V006

--- Notificacao.cs
+++ Notificacao.cs
@@ -10,31 +10,32 @@ namespace FRS_Biblioteca
         public static void Envianotificação(string texto)
         {
             FRS_Biblioteca.DB.GravaAtividade(texto);
-            var configuracao = ConfigurationManager.AppSettings;
-            PushbulletClient client = new PushbulletClient("o.ijhfmbKRI8JAAAjaorTvs3n1I1oHo4qH");
-            //var devices = client.CurrentUsersDevices();
-            //var device = devices.Devices.Where(o => o.Manufacturer == "Apple").FirstOrDefault();
-            if (configuracao["celular1"] == "Yes")
+            if (FRS_Biblioteca.GlobalVar.UsuarioLogado.grupo != "Admin")
             {
-                PushNoteRequest request1 = new PushNoteRequest
+                var configuracao = ConfigurationManager.AppSettings;
+                PushbulletClient client = new PushbulletClient("o.ijhfmbKRI8JAAAjaorTvs3n1I1oHo4qH");
+                if (configuracao["celular1"] == "Yes")
                 {
-                    DeviceIden = "ujy7mvFtukmsjE5NB4jvkO",
-                    Title = texto,
-                    Body = texto
-                };
+                    PushNoteRequest request1 = new PushNoteRequest
+                    {
+                        DeviceIden = "ujy7mvFtukmsjE5NB4jvkO",
+                        Title = texto,
+                        Body = texto
+                    };
 
-                PushResponse response1 = client.PushNote(request1);
-            }
+                    PushResponse response1 = client.PushNote(request1);
+                }
 
-            if (configuracao["celular2"] == "Yes")
-            {
-                PushNoteRequest request2 = new PushNoteRequest
+                if (configuracao["celular2"] == "Yes")
                 {
-                    DeviceIden = "ujy7mvFtukmsjBmQj6ywIS",
-                    Title = texto,
-                    Body = texto
-                };
-                PushResponse response2 = client.PushNote(request2);
+                    PushNoteRequest request2 = new PushNoteRequest
+                    {
+                        DeviceIden = "ujy7mvFtukmsjBmQj6ywIS",
+                        Title = texto,
+                        Body = texto
+                    };
+                    PushResponse response2 = client.PushNote(request2);
+                }
             }
         }
     }
