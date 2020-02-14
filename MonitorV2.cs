3c4b44f790168619a317327d9793a6abd00e1fd7
henriqueserra <52676788+henriqueserra@users.noreply.github.com>
2020-02-13 21:15:15 -0300

    V007

---
+++ MonitorV2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRS_Biblioteca
+{
+    public class MonitorV2
+    {
+        public static void ChecaMonitorV2()
+        {
+            Process[] localAll = Process.GetProcesses();
+            foreach (var item in localAll)
+            {
+                if (!item.ProcessName.Contains("MonitorV2"))
+                {
+                    FRS_Biblioteca.Notificacao.Envianotificação("Monitor não está em execução !!!");
+                }
+            }
+        }
+    }
+}
